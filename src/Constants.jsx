86470f46678610bfd7d4b2a1c7554ec158d25718
kaarthie <karthironaldo989@gmail.com>
2024-01-24 18:24:11 +0530

    initial commit - refactored

---
+++ src/Constants.jsx
@@ -0,0 +1,13 @@
+// const URL='';
+const CHAT_API = import.meta.env.VITE_APP_CHAT_API;
+const SEARCH_API = import.meta.env.VITE_APP_SEARCH_API;
+const PRICE_DETAILS = {
+  superior: 7000,
+  business: 8000,
+  junior: 10000,
+  suite: 21000,
+  presidential: 50000,
+  deluxe: 51000,
+  superdeluxe: 55000,
+};
+export { CHAT_API, SEARCH_API, PRICE_DETAILS };
